0365e220e9886bee90794ed77063282af5fe9bb8
Yana Horbenko <yanagorbenko33@gmail.com>
2026-06-25 16:59:47 +0300

    add cors

--- src/server.js
+++ src/server.js
@@ -10,10 +10,12 @@ import { errors } from 'celebrate';
 import selectedRouter from './routers/selectedGamesRouter.js';
 import ideasRouter from './routers/ideasRouter.js';
 import userRouter from './routers/usersRouter.js';
+import cors from 'cors';
 
 const PORT = process.env.PORT;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
