f9e316e6392595a35a466f69a3aee6d0c94d718b
Yana Horbenko <yanagorbenko33@gmail.com>
2026-06-28 12:11:04 +0300

    add trust proxy

--- src/server.js
+++ src/server.js
@@ -16,6 +16,8 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.set('trust proxy', 1);
+
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
@@ -24,7 +26,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: allowedOrigins, // ✅ Просто массив
+    origin: allowedOrigins,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
